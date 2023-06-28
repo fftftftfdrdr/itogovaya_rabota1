adeb4d8b7fa0cade54085c9d7c676b5b5735c2f8
Ivan Slukin <provaider010@gmail.com>
2023-06-28 20:05:26 +1000

    added dicription for code of task

--- KOD/Program.cs
+++ KOD/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"da", "ghgh", "hello", "world", "teftelka"};
+﻿string[] array1 = new string[5] {"da", "ghg", "hello", "world", "teftelka"};
 string[] array2 = new string[array1.Length];
 void SokrashenieArray(string[] array1, string[] array2)
 {
